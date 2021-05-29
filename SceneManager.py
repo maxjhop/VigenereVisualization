c58551435e3fd1c48b8207bd46715ed543e72613
maxjhop <maxjhop99@gmail.com>
2021-05-28 22:34:30 -0700

    fixed buttons

--- SceneManager.py
+++ SceneManager.py
@@ -457,7 +457,7 @@ class StartMenu(SceneManager):
         self.quit = self.smallfont.render('Quit', True, self.color)
 
         menu_button = button(self.height / 4.2, self.menu, lambda: self.SwitchToScene(MainMenu()), self.width / 2 - 140, 250, 40)
-        info_button = button(self.height / 2.7, self.info, lambda: print("info"), self.width / 2 - 140, 250, 40, 100)
+        info_button = button(self.height / 2.7, self.info, lambda: self.SwitchToScene(InfoMenu()), self.width / 2 - 140, 250, 40, 100)
         quit_button = button(self.height / 2, self.quit, lambda : pygame.quit(), self.width / 2 - 140, 250, 40, 100)
 
         self.buttons = [menu_button, info_button, quit_button]
@@ -481,3 +481,373 @@ class StartMenu(SceneManager):
         for i in self.buttons:
             i.draw(screen)
         screen.blit(self.title, (self.width / 2 - 200, self.height / 8))
+
+class InfoMenu(SceneManager):
+    def __init__(self):
+        SceneManager.__init__(self)
+        print("Information")
+
+        # white color
+        self.color = (255, 255, 255)
+
+        # light shade of the button
+        self.color_light = (170, 170, 170)
+
+        # dark shade of the button
+        self.color_dark = (100, 100, 100)
+
+        # stores the width of the
+        # screen into a variable
+        self.width = 720
+
+        # stores the height of the
+        # screen into a variable
+        self.height = 720
+
+        self.x2 = 75
+
+        self.y2 = 30
+        # defining a font
+        self.smallfont = pygame.font.SysFont('Corbel', 35)
+        self.input_smallfont = pygame.font.SysFont('Corbel', 24)
+
+        # rendering a text written in
+        # this font
+        self.about = self.smallfont.render('About Ciphers', True, self.color)
+        self.use = self.smallfont.render('About This Tool', True, self.color)
+        self.menu = self.smallfont.render("Main Menu", True, self.color)
+        self.information = self.smallfont.render("Information", True, self.color_dark)
+
+        # storing the x1 value so buttons will be center screen
+        midX = self.width - 75/2
+
+        # buttons and their locations/functions
+        menu_button = button(self.height/2 - self.y2*1.5 +5, self.menu, lambda: self.SwitchToScene(StartMenu()), x1 = midX)
+        about_button = button(self.height/2 - self.y2/2, self.about, lambda: self.SwitchToScene(AboutCi_1()), x1 = midX)
+        use_button = button(self.height/2 + self.y2*1.5 +5, self.use, lambda: self.SwitchToScene(Uses_1()), x1 = midX)
+
+        self.buttons = [menu_button, about_button, use_button]
+
+    def Input(self, events, pressed_keys, mouse):
+        for ev in events:
+            """
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            """
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on a
+                # button the game does the thing
+                for i in self.buttons:
+                    if i.hover(mouse):
+                        i.click()
+
+    def Render(self, screen, mouse):
+        # fills the screen with a color
+        screen.fill((255, 255, 165))
+
+        for i in self.buttons:
+            i.draw(screen)
+
+        #Menu title
+        screen.blit(self.information, (self.width / 2.5, self.height / 3.5))
+
+    def update(self, board, size):
+        return None
+
+class Uses_1(SceneManager):
+    def __init__(self):
+        SceneManager.__init__(self)
+        print("Information")
+
+        # white color
+        self.color = (255, 255, 255)
+
+        # light shade of the button
+        self.color_light = (170, 170, 170)
+
+        # dark shade of the button
+        self.color_dark = (100, 100, 100)
+
+        # stores the width of the
+        # screen into a variable
+        self.width = 720
+
+        # stores the height of the
+        # screen into a variable
+        self.height = 720
+
+        self.x2 = 75
+
+        self.y2 = 30
+
+        # defining a font
+        self.smallfont = pygame.font.SysFont('Corbel', 35)
+        self.input_smallfont = pygame.font.SysFont('Corbel', 24)
+
+        # rendering a text written in
+        # this font
+        self.next = self.smallfont.render('Next (Controls)', True, self.color)
+        self.menu = self.smallfont.render("Main Menu", True, self.color)
+        self.title = self.smallfont.render("Using this tool", True, self.color_dark)
+
+
+        # buttons and their locations/functions
+        menu_button = button(self.height - 20, self.menu, lambda: self.SwitchToScene(StartMenu()), x1 = 20)
+        next_button = button(self.height - 20, self.next, lambda: self.SwitchToScene(Uses_2()), x1 = self.width - 20 - self.x2)
+
+        self.buttons = [menu_button, next_button]
+
+    def Input(self, events, pressed_keys, mouse):
+        for ev in events:
+            """
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            """
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on a
+                # button the game does the thing
+                for i in self.buttons:
+                    if i.hover(mouse):
+                        i.click()
+
+    def Render(self, screen, mouse):
+        # fills the screen with a color
+        screen.fill((255, 255, 165))
+
+        for i in self.buttons:
+            i.draw(screen)
+
+        #Title
+        screen.blit(self.title, (self.width / 2.5, self.height / 3.5))
+
+        #Information
+
+    def update(self, board, size):
+        return None
+
+class Uses_2(SceneManager):
+    def __init__(self):
+        SceneManager.__init__(self)
+        print("Information")
+
+        # white color
+        self.color = (255, 255, 255)
+
+        # light shade of the button
+        self.color_light = (170, 170, 170)
+
+        # dark shade of the button
+        self.color_dark = (100, 100, 100)
+
+        # stores the width of the
+        # screen into a variable
+        self.width = 720
+
+        # stores the height of the
+        # screen into a variable
+        self.height = 720
+
+        self.x2 = 75
+
+        self.y2 = 30
+
+        # defining a font
+        self.smallfont = pygame.font.SysFont('Corbel', 35)
+        self.input_smallfont = pygame.font.SysFont('Corbel', 24)
+
+        # rendering a text written in
+        # this font
+        self.back = self.smallfont.render('Back', True, self.color)
+        self.menu = self.smallfont.render("Main Menu", True, self.color)
+        self.next = self.smallfont.render("Next", True, self.color)
+        self.title = self.smallfont.render("Using this tool2", True, self.color_dark)
+
+
+        # buttons and their locations/functions
+        menu_button = button(self.height - 20, self.menu, lambda: self.SwitchToScene(StartMenu()), x1 = 20)
+        back_button = button(self.height - 20, self.next, lambda: self.SwitchToScene(Uses_1()), x1 = self.width - 20 - self.x2)
+
+        self.buttons = [menu_button, back_button]
+
+    def Input(self, events, pressed_keys, mouse):
+        for ev in events:
+            """
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            """
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on a
+                # button the game does the thing
+                for i in self.buttons:
+                    if i.hover(mouse):
+                        i.click()
+
+    def Render(self, screen, mouse):
+        # fills the screen with a color
+        screen.fill((255, 255, 165))
+
+        for i in self.buttons:
+            i.draw(screen)
+
+        #Title
+        screen.blit(self.title, (self.width / 2.5, self.height / 3.5))
+
+        #Information
+
+    def update(self, board, size):
+        return None
+
+
+class AboutCi_1(SceneManager):
+    def __init__(self):
+        SceneManager.__init__(self)
+        print("Information")
+
+        # white color
+        self.color = (255, 255, 255)
+
+        # light shade of the button
+        self.color_light = (170, 170, 170)
+
+        # dark shade of the button
+        self.color_dark = (100, 100, 100)
+
+        # stores the width of the
+        # screen into a variable
+        self.width = 720
+
+        # stores the height of the
+        # screen into a variable
+        self.height = 720
+
+        self.x2 = 75
+
+        self.y2 = 30
+
+        # defining a font
+        self.smallfont = pygame.font.SysFont('Corbel', 35)
+        self.input_smallfont = pygame.font.SysFont('Corbel', 24)
+
+        # rendering a text written in
+        # this font
+        self.next = self.smallfont.render('Next (Vigenere Cipher)', True, self.color)
+        self.menu = self.smallfont.render("Main Menu", True, self.color)
+        self.ciphers = self.smallfont.render("Ciphers", True, self.color_dark)
+
+
+        # buttons and their locations/functions
+        menu_button = button(self.height - 20, self.menu, lambda: self.SwitchToScene(StartMenu()), x1 = 20)
+        next_button = button(self.height - 20, self.next, lambda: self.SwitchToScene(AboutCi_2()), x1 = self.width - 20 - self.x2)
+
+        self.buttons = [menu_button, next_button]
+
+    def Input(self, events, pressed_keys, mouse):
+        for ev in events:
+            """
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            """
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on a
+                # button the game does the thing
+                for i in self.buttons:
+                    if i.hover(mouse):
+                        i.click()
+
+    def Render(self, screen, mouse):
+        # fills the screen with a color
+        screen.fill((255, 255, 165))
+
+        for i in self.buttons:
+            i.draw(screen)
+
+        #Title
+        screen.blit(self.ciphers, (self.width / 2.5, self.height / 3.5))
+
+        #Information
+
+    def update(self, board, size):
+        return None
+
+
+class AboutCi_2(SceneManager):
+    def __init__(self):
+        SceneManager.__init__(self)
+        print("Information")
+
+        # white color
+        self.color = (255, 255, 255)
+
+        # light shade of the button
+        self.color_light = (170, 170, 170)
+
+        # dark shade of the button
+        self.color_dark = (100, 100, 100)
+
+        # stores the width of the
+        # screen into a variable
+        self.width = 720
+
+        # stores the height of the
+        # screen into a variable
+        self.height = 720
+
+        self.x2 = 75
+
+        self.y2 = 30
+
+        # defining a font
+        self.smallfont = pygame.font.SysFont('Corbel', 35)
+        self.input_smallfont = pygame.font.SysFont('Corbel', 24)
+
+        # rendering a text written in
+        # this font
+        self.back = self.smallfont.render('Back', True, self.color)
+        self.menu = self.smallfont.render("Main Menu", True, self.color)
+        self.title = self.smallfont.render("The Vigenere Cipher", True, self.color_dark)
+        self.next = self.smallfont.render('Next', True, self.color)
+
+
+        # buttons and their locations/functions
+        menu_button = button(self.height - 20, self.menu, lambda: self.SwitchToScene(StartMenu()), x1 = 20)
+        back_button = button(self.height - 20, self.next, lambda: self.SwitchToScene(AboutCi_1()), x1 = self.width - 20 - self.x2)
+
+        self.buttons = [menu_button, back_button]
+
+    def Input(self, events, pressed_keys, mouse):
+        for ev in events:
+            """
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            """
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on a
+                # button the game does the thing
+                for i in self.buttons:
+                    if i.hover(mouse):
+                        i.click()
+
+    def Render(self, screen, mouse):
+        # fills the screen with a color
+        screen.fill((255, 255, 165))
+
+        for i in self.buttons:
+            i.draw(screen)
+
+        #Title
+        screen.blit(self.title, (self.width / 2.5, self.height / 3.5))
+
+        #Information
+
+    def update(self, board, size):
+        return None
